27118d196fc87c23af04ff277cd3967cee5468c1
SphereStacking <sphere.stacking@gmail.com>
2024-09-29 02:35:41 +0900

    deploy script fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,72 @@
 #!/bin/bash -e
 
-description=$1
+latest=$1
 
 # コードをGASにpush
 clasp push --force
 
-# 最新のデプロイIDを取得
-LATEST_DEPLOYMENT_ID=$(clasp deployments | grep -oP '(?<=- )AKfycb[^\s]+' | head -n 1)
+# デプロイIDのリストを取得
+DEPLOYMENT_IDS=$(clasp deployments | grep -oP '(?<=- )AKfycb[^\s]+')
 
 # デプロイIDが見つからない場合
-if [ -z "$LATEST_DEPLOYMENT_ID" ]; then
-  echo "Error: No deployment ID found."
+if [ -z "$DEPLOYMENT_IDS" ]; then
+  echo "Error: No deployment IDs found."
   exit 1
 fi
 
-# 新しいバージョンを作成
-clasp version $description
+# デプロイIDを選択形式で表示
+select_deployment_id() {
+  if [ "$latest" == "latest" ]; then
+    echo "$DEPLOYMENT_IDS" | tail -n 1
+  else
+    echo "Select a deployment ID:"
+    select id in $DEPLOYMENT_IDS; do
+      if [ -n "$id" ]; then
+        echo "$id"
+        break
+      else
+        echo "Invalid selection. Please try again."
+      fi
+    done
+  fi
+}
 
 # 最新のバージョン番号を取得
-LATEST_VERSION=$(clasp versions | grep -oP '^\d+' | head -n 1)
+get_latest_version() {
+  local versions=$(clasp versions | grep -oP '^\d+')
+  if [ -z "$versions" ]; then
+    echo "Error: バージョン番号が見つかりません。"
+    exit 1
+  fi
 
-# バージョン番号が見つからない場合
-if [ -z "$LATEST_VERSION" ]; then
-  echo "Error: バージョン番号が見つかりません。"
-  exit 1
-fi
+  if [ "$latest" == "latest" ]; then
+    echo "$versions" | tail -n 1
+  else
+    echo "Select a version number:"
+    select version in $versions; do
+      if [ -n "$version" ]; then
+        echo "$version"
+        break
+      else
+        echo "Invalid selection. Please try again."
+      fi
+    done
+  fi
+}
+
+LATEST_DEPLOYMENT_ID=$(select_deployment_id)
+
+# 最新のバージョンを作成
+clasp version  
+
+LATEST_VERSION=$(get_latest_version)
+
+echo "LATEST_DEPLOYMENT_ID: $LATEST_DEPLOYMENT_ID"
+echo "LATEST_VERSION: $LATEST_VERSION"
+
+echo "clasp deploy -i ${LATEST_DEPLOYMENT_ID} -V ${LATEST_VERSION}"
 
 # 最新のバージョンを最新のデプロイIDにデプロイ
 clasp deploy -i "$LATEST_DEPLOYMENT_ID" -V "$LATEST_VERSION"
 
-echo "デプロイ成功🟢: $NEW_DEPLOYMENT_ID"
+echo "デプロイ成功🟢: $LATEST_DEPLOYMENT_ID"
